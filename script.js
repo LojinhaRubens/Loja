56cfc6cb49a82fe8f1696e5569c3c166767b3e6c
Rubens Herculano <rubensherculano@hotmail.com>
2025-05-17 13:33:11 -0300

    Pequenos Ajustes

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-document.querySelectorAll('.item').forEach(card => {
-    const images = card.querySelector('.carousel-images');
-    const prevBtn = card.querySelector('.carousel-btn.prev');
-    const nextBtn = card.querySelector('.carousel-btn.next');
-    let index = 0;
-    const total = images.children.length;
-
-    function updateCarousel() {
-      images.style.transform = `translateX(-${index * 100}%)`;
-    }
-
-    prevBtn.addEventListener('click', () => {
-      index = (index - 1 + total) % total;
-      updateCarousel();
-    });
-
-    nextBtn.addEventListener('click', () => {
-      index = (index + 1) % total;
-      updateCarousel();
-    });
+new Swiper('.swiper', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true
 });
+
+
+
